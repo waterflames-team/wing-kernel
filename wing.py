c34c1a815c09a8b839bd7f9a02ce392b06544e37
FFXW0720 <2822603942@qq.com>
2020-04-15 19:33:58 +0800

    对上一个版本model找不到的bug进行修复

--- wing.py
+++ wing.py
@@ -44,32 +44,35 @@ if len(sys.argv) == 1:#无的情况
 
 
 if server_begin == 0:#判断无的情况是否已经经过，避免不必要的运行错误
+    
     model = sys.argv[1]#如果前面的if通过，说明有东西，那么可以开始检测后面的内容
+
+    if model == "s":#配置生效
+        print("激活配置生效")
+        sys.exit(-1)#退出
+    if model == "z":#使用指南
+        print("使用指南：")
+        print('''
+        输入python3 wing.py XXX中XXX部分的东西
+        s为激活配置生效
+        z为使用指南
+        v为版本号
+        无为介绍同时打开后台的服务器
+        ''')
+        sys.exit(-1)#退出
+    if model == "v":#版本号
+        print("版本号：")
+        print('''
+        目前程序开发中
+        所有的开发版本都会在第三位做文章
+        开发版本0.0.8（01版将完成部分东西的修改，不支持新增文章，02将重构)
+        ''')
+        sys.exit(-1)#退出
+    else:#错误的情况
+        print("请确认你的激活方式是否错误")
+        sys.exit(-1)#退出
+
 else:
     pass
 
 
-if model == "s":#配置生效
-    print("激活配置生效")
-    sys.exit(-1)#退出
-if model == "z":#使用指南
-    print("使用指南：")
-    print('''
-    输入python3 wing.py XXX中XXX部分的东西
-    s为激活配置生效
-    z为使用指南
-    v为版本号
-    无为介绍同时打开后台的服务器
-    ''')
-    sys.exit(-1)#退出
-if model == "v":#版本号
-    print("版本号：")
-    print('''
-    目前程序开发中
-    所有的开发版本都会在第三位做文章
-    开发版本0.0.7（01版将完成部分东西的修改，不支持新增文章，02将重构)
-    ''')
-    sys.exit(-1)#退出
-else:#错误的情况
-    print("请确认你的激活方式是否错误")
-    sys.exit(-1)#退出
