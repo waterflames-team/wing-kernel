e2102ad2169c6883bd4c66a83900fadf0730b198
FFXW0720 <2822603942@qq.com>
2020-04-15 19:31:08 +0800

    修改注释，增加判定无的情况是否通过

--- wing.py
+++ wing.py
@@ -9,8 +9,9 @@ import sys
 #引入文件
 import con
 
-#准备文件
+#准备数据
 theme = con.theme()
+server_begin = 0
 
 '''
 函数区：
@@ -32,17 +33,26 @@ v为版本号
 
 print("欢迎使用wing")
 
-#无的情况
-if len(sys.argv) == 1:
+if len(sys.argv) == 1:#无的情况
     print("介绍")#同时打开后台服务器
-    sys.exit(-1)#退出
+    print('''
+    灵翼是一个由非凡小王开发，折腾调协助维护的基于python3语言的一键建站系统
+    您运行此步同时会启动后台
+    如要关闭请按ctrl+4
+    ''')
+    server_begin = 1
 
-model = sys.argv[1]#如果前面的if通过，说明有东西，那么可以开始检测后面的内容
 
-if model == "s":
+if server_begin == 0:#判断无的情况是否已经经过，避免不必要的运行错误
+    model = sys.argv[1]#如果前面的if通过，说明有东西，那么可以开始检测后面的内容
+else:
+    pass
+
+
+if model == "s":#配置生效
     print("激活配置生效")
     sys.exit(-1)#退出
-if model == "z":
+if model == "z":#使用指南
     print("使用指南：")
     print('''
     输入python3 wing.py XXX中XXX部分的东西
@@ -52,14 +62,14 @@ if model == "z":
     无为介绍同时打开后台的服务器
     ''')
     sys.exit(-1)#退出
-if model == "v":
+if model == "v":#版本号
     print("版本号：")
     print('''
     目前程序开发中
     所有的开发版本都会在第三位做文章
-    开发版本0.0.6（01版将完成部分东西的修改，不支持新增文章，02将重构)
+    开发版本0.0.7（01版将完成部分东西的修改，不支持新增文章，02将重构)
     ''')
     sys.exit(-1)#退出
-else:
+else:#错误的情况
     print("请确认你的激活方式是否错误")
     sys.exit(-1)#退出
