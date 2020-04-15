e3bf556538ce7d1c2a0c73288aafbe196db2a537
FFXW0720 <2822603942@qq.com>
2020-04-15 18:31:11 +0800

    实现几个不同的激活方式：s、z、v、无、其他

--- wing.py
+++ wing.py
@@ -24,10 +24,30 @@ def exh():
 '''
 运行区：
 以sys组件读取python3 wing.py XXX中XXX部分的东西
-s为保存
+s为激活配置生效
 z为使用指南
 v为版本号
-无为介绍
+无为介绍同时打开后台的服务器
 '''
 
 print("欢迎使用wing")
+
+#无的情况
+if len(sys.argv) == 1:
+    print("介绍")#同时打开后台服务器
+    sys.exit(-1)#退出
+    
+model = sys.argv[1]#如果前面的if通过，说明有东西，那么可以开始检测后面的内容
+
+if model == "s":
+    print("激活配置生效")
+    sys.exit(-1)#退出
+if model == "z":
+    print("使用指南")
+    sys.exit(-1)#退出
+if model == "v":
+    print("版本号")
+    sys.exit(-1)#退出
+else:
+    print("请确认你的激活方式是否错误")
+    sys.exit(-1)#退出
