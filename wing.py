706f1f965c3e2b8a6d3fc10ce6f88a8bac6d3b46
FFXW0720 <2822603942@qq.com>
2020-04-15 17:19:34 +0800

    代码规划

--- wing.py
+++ wing.py
@@ -1,1 +1,23 @@
-print("hello word")
+import sys
+import json
+
+
+
+'''
+函数区：
+命名以主题前三个字幕为准
+'''
+def exh():
+    print("检测到主题blog-exhibition")
+
+
+'''
+运行区：
+以sys组件读取python3 wing.py XXX中XXX部分的东西
+s为保存
+z为使用指南
+v为版本号
+无为介绍
+'''
+
+print("欢迎使用wing")
