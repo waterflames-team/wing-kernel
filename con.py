779e307827622e026adbaa2aec72cfaf4e2206b0
FFXW0720 <2822603942@qq.com>
2020-04-18 14:08:37 +0800

    写好con的读取方法

--- con.py
+++ con.py
@@ -13,3 +13,19 @@ def theme():
     conf_jg = conf_jg[thing]
 
     return conf_jg
+
+def config(theme,xiang):
+    theme_c = theme
+    xiang_c = xiang
+
+    conf_z = open(theme_c+"config.json",'r')
+    conf_s = conf_z.read()
+    conf_z.close()
+
+    conf_j = str(conf_s)
+
+    thing = xiang#取xx项
+    conf_jg = json.loads(conf_j)
+    conf_jg = conf_jg[thing]
+
+    return conf_jg
